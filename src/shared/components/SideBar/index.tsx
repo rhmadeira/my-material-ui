b8d90ee0fb64df6c69b4033dd3f6243b8c912af1
Rafael Henrique Madeira Feitosa <rafael.henrique.mad@gmail.com>
2023-02-03 23:06:35 -0300

    feat: complement

--- src/shared/components/SideBar/index.tsx
+++ src/shared/components/SideBar/index.tsx
@@ -1,4 +1,8 @@
-import { Drawer, useTheme } from "@mui/material";
+import { Avatar, Drawer, Icon, List, useTheme } from "@mui/material";
+import Divider from "@mui/material/Divider";
+import { ListItemButton } from "@mui/material";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
 import { Box } from "@mui/system";
 
 interface ISideBarProps {
@@ -9,7 +13,39 @@ export const SideBar = ({ children }: ISideBarProps) => {
   const theme = useTheme();
   return (
     <>
-      <Drawer variant="permanent">Teste</Drawer>
+      <Drawer variant="permanent">
+        <Box
+          width={theme.spacing(28)}
+          height="100%"
+          display="flex"
+          flexDirection="column"
+        >
+          <Box
+            width="100%"
+            height={theme.spacing(20)}
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Avatar
+              alt="Avatar"
+              src="https://images.freeimages.com/vhq/images/previews/248/vintage-logo-mockup-psd-397079.jpg"
+              sx={{ width: theme.spacing(12), height: theme.spacing(12) }}
+            />
+          </Box>
+          <Divider />
+          <Box flex={1}>
+            <List component="nav">
+              <ListItemButton>
+                <ListItemIcon>
+                  <Icon>home</Icon>
+                </ListItemIcon>
+                <ListItemText primary="Home" />
+              </ListItemButton>
+            </List>
+          </Box>
+        </Box>
+      </Drawer>
       <Box height="100vh" marginLeft={theme.spacing(28)}>
         {children}
       </Box>
